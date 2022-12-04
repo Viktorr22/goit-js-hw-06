54cd891413e2b291fb4de1bdc2a28bc3c372aeeb
Viktorr22 <progdub1@gmail.com>
2022-12-04 21:48:13 +0200

    task9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() {  
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+console.log(getRandomHexColor());
