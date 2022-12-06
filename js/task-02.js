df6196a30c764d0d99b9e5e8255fd90969c87da0
Viktorr22 <progdub1@gmail.com>
2022-12-06 21:01:20 +0200

    task2 correct

--- js/task-02.js
+++ js/task-02.js
@@ -9,29 +9,13 @@ const ingredients = [
 
 const list = document.querySelector('#ingredients');
 
-const potatoesItem = document.createElement('li');
-potatoesItem.textContent = 'Potatoes';
-potatoesItem.classList.add('item');
+const ingredientsArr = ingredients.map((element) => {
+const ingredientsItem = document.createElement('li');
+ingredientsItem.textContent = element;
+ingredientsItem.classList.add('item');
+  return ingredientsItem; 
+ 
+}) 
 
-const mushroomsItem = document.createElement('li');
-mushroomsItem.textContent = 'Mushrooms';
-mushroomsItem.classList.add('item');
-
-const garlicItem = document.createElement('li');
-garlicItem.textContent = 'Garlic';
-garlicItem.classList.add('item');
-
-const tomatosItem = document.createElement('li');
-tomatosItem.textContent = 'Tomatos';
-tomatosItem.classList.add('item');
-
-const herbsItem = document.createElement('li');
-herbsItem.textContent = 'Herbs';
-herbsItem.classList.add('item');
-
-const condimentsItem = document.createElement('li');
-condimentsItem.textContent = 'Condiments';
-condimentsItem.classList.add('item');
-
-list.append(potatoesItem, mushroomsItem, garlicItem, tomatosItem, herbsItem, condimentsItem);
+list.append(...ingredientsArr);
 console.log(list);
