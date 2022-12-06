d7e03742e16494f7cd8e7a3040441f35ac0cced8
Viktorr22 <progdub1@gmail.com>
2022-12-06 21:23:00 +0200

    task5 correct

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,12 @@ const textInput = document.querySelector('#name-input');
 const textOutput = document.querySelector('#name-output');
 
 textInput.addEventListener("input", (event) => {
-    textOutput.textContent = event.currentTarget.value;
+    if (event.currentTarget.value != "") {
+       return textOutput.textContent = event.currentTarget.value; 
+    }
+    textOutput.textContent = "Anonymous";
 });
+
+
+
+
