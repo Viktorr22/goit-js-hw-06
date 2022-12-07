0ef1db2fc0ba94aabc7c4fb6864b28b1c7e09316
Viktorr22 <progdub1@gmail.com>
2022-12-07 15:34:40 +0200

    task 6 correct

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,7 @@
 const thisInput = document.querySelector('input');
 
 function checkInput() {
+    thisInput.classList.remove("valid", "invalid")
     if (thisInput.value.length === Number(thisInput.dataset.length)) {
         thisInput.classList.add("valid");
     }
